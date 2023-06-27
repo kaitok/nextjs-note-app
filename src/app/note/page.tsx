0563cca6a1c562ed6c8544824ffc15dd046e8567
jo <kaitokinjo@gmail.com>
2023-06-27 09:39:01 +0900

    add note

---
+++ src/app/note/page.tsx
@@ -0,0 +1,19 @@
+export default function Notes() {
+  return (
+    <>
+      <h1>Create Note</h1>
+      <textarea
+        id="message"
+        rows="4"
+        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Leave a comment..."
+      ></textarea>
+      <button
+        type="button"
+        class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+      >
+        Submit
+      </button>
+    </>
+  )
+}
