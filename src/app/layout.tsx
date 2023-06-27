0c9fd2fcb07c5866fe795f0e1559ea0f16890a51
jo <kaitokinjo@gmail.com>
2023-06-27 09:38:48 +0900

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="p-5">{children}</div>
+      </body>
     </html>
   )
 }
