cd1e20ecd04c97c64ffbabeef9a7727bcdac9d0e
jo <kaitokinjo@gmail.com>
2023-07-02 23:28:45 +0900

    add title

--- src/app/note/page.tsx
+++ src/app/note/page.tsx
@@ -1,17 +1,27 @@
 export default function Notes() {
   return (
     <>
-      <div className="flex flex-col gap-1">
+      <div className="flex flex-col gap-4">
         <div className="mb-2">
           <h1 className="font-medium">Create Note</h1>
         </div>
         <div>
+          <label className="label">
+            <span className="label-text">Title</span>
+          </label>
+          <input type="text" className="input input-bordered w-full"></input>
+        </div>
+        <div>
+          <label className="label">
+            <span className="label-text">Content</span>
+          </label>
           <textarea
             className="textarea textarea-bordered w-full"
-            rows="20"
+            rows="10"
             placeholder="Note"
           ></textarea>
         </div>
+
         <div>
           <button
             type="button"
