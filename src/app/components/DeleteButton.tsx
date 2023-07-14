812586ea03ed628df0e333380718e89c7cc1cc36
jo <kaitokinjo@gmail.com>
2023-07-14 20:46:15 +0900

    update edit page server action

--- src/app/components/DeleteButton.tsx
+++ src/app/components/DeleteButton.tsx
@@ -1,26 +1,26 @@
-'use client'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
-import { useRouter } from 'next/navigation'
-import { deleteData } from '../utils/apiRequest'
+import { redirect } from 'next/navigation'
+import { deleteQuery } from '../utils/prismaQuery'
 
 export default function DeleteButton({ id }: { id: string }) {
-  const router = useRouter()
-
-  const handleDelete = async () => {
-    await deleteData('notes', id)
-    router.replace('/')
+  const handleSubmit = async () => {
+    'use server'
+    await deleteQuery('note', id)
+    redirect('/')
   }
+
   return (
     <>
-      <button
-        type="button"
-        className="flex items-center gap-1 py-2 px-4 text-xs font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-200"
-        onClick={handleDelete}
-      >
-        <FontAwesomeIcon icon={faTrashCan} className="h-[12px]" />
-        Delete
-      </button>
+      <form action={handleSubmit}>
+        <button
+          type="submit"
+          className="flex items-center gap-1 py-2 px-4 text-xs font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-200"
+        >
+          <FontAwesomeIcon icon={faTrashCan} className="h-[12px]" />
+          Delete
+        </button>
+      </form>
     </>
   )
 }
--- src/app/notes/[id]/edit/page.tsx
+++ src/app/notes/[id]/edit/page.tsx
@@ -1,60 +1,21 @@
-'use client'
-import { useState, useEffect } from 'react'
-import { patchData, getData } from '@/app/utils/apiRequest'
-import { useRouter } from 'next/navigation'
 import { Note } from '@/app/types/note'
+import { getQuery, updateQuery } from '@/app/utils/prismaQuery'
+import { redirect } from 'next/navigation'
 
-export default function Note({ params }: { params: { id: string } }) {
-  const [data, setData] = useState<undefined | null | Note>(undefined)
-  const [formData, setFormData] = useState({
-    title: '',
-    content: '',
-  })
-  const router = useRouter()
+export default async function Note({ params }: { params: { id: string } }) {
   const { id } = params
+  const data = await getQuery('note', id)
 
-  const fetchData = async () => {
-    const res = await getData('notes', { id })
-    if (res.body.length !== 0) {
-      setFormData(res.body[0])
-    } else {
-      setFormData({})
-    }
+  async function handleSubmit(formData: FormData) {
+    'use server'
+    updateQuery('note', id, formData)
+    redirect('/notes/' + id)
   }
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    })
-  }
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-
-    const updatedDate = new Date().toISOString()
-    const requestData = {
-      ...formData,
-      updated_date: updatedDate,
-    }
-
-    try {
-      const res = await patchData('notes', requestData)
-      console.log('Form submitted successfully')
-      router.replace(`/notes/${id}`)
-    } catch (error) {
-      console.error('Error while submitting form:', error)
-    }
-  }
-
-  useEffect(() => {
-    fetchData()
-  }, [])
 
   return (
     <>
       <div className="flex flex-col gap-4">
-        <form onSubmit={handleSubmit}>
+        <form action={handleSubmit}>
           <div className="mb-2">
             <h1 className="font-medium">Edit Note #{params.id}</h1>
           </div>
@@ -66,8 +27,7 @@ export default function Note({ params }: { params: { id: string } }) {
               type="text"
               className="input input-bordered w-full"
               name="title"
-              value={formData.title}
-              onChange={handleChange}
+              defaultValue={data.title}
             ></input>
           </div>
           <div>
@@ -79,8 +39,7 @@ export default function Note({ params }: { params: { id: string } }) {
               rows="10"
               placeholder="Note"
               name="content"
-              value={formData.content}
-              onChange={handleChange}
+              defaultValue={data.content}
             ></textarea>
           </div>
           <div className="text-right">
