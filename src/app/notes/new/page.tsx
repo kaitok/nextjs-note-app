ca00238313d7301c9fb4fa250f29c7d48a5a36b5
jo <kaitokinjo@gmail.com>
2023-07-14 20:47:39 +0900

    remove json-server

--- src/app/notes/new/page.tsx
+++ src/app/notes/new/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState } from 'react'
-import { postData } from '@/app/utils/apiRequest'
+// import { postData } from '@/app/utils/apiRequest'
 import { useRouter } from 'next/navigation'
 
 export default function Notes() {
